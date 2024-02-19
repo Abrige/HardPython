0f12eea5329b0b59eec1235db5a3aa4afce16c46
Abrige!04 <mattiabrizi94@gmail.com>
2024-02-19 21:31:02 +0100

    Prova cmd

---
+++ LezioniPrima/terzoScript.py
@@ -0,0 +1,10 @@
+# come mettere le virgolette all'interno di una stringa
+txt = "Ciao sono \"Mattia\""
+print(txt)
+txt1 = 'Ciao sono \"Mattia\"'
+print(txt1)
+secondTxt = "\nCiao sono Mattia ma a capo"
+print(secondTxt)
+
+# operatore in molto utile
+print("Ciao" in txt1)
